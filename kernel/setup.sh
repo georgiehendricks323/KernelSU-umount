432086f7445f3aa80fac142868a45ce4d62cdf99
georgiehendricks323 <161995698+georgiehendricks323@users.noreply.github.com>
2024-03-04 18:20:03 +0800

    Update setup.sh

--- kernel/setup.sh
+++ kernel/setup.sh
@@ -15,7 +15,7 @@ else
      exit 127
 fi
 
-test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/georgiehendricks323/KernelSU-umount
+test -d "$GKI_ROOT/KernelSU" || git clone https://github.com/georgiehendricks323/KernelSU-umount KernelSU
 cd "$GKI_ROOT/KernelSU"
 git stash
 if [ "$(git status | grep -Po 'v\d+(\.\d+)*' | head -n1)" ]; then
